9e22d50bdbf0c8b2286ac2ef5288fc5722ac7402
Diego Lehyt <diegolehy00@gmail.com>
2020-04-03 17:52:05 -0400

    missing, resume, link, foto and text

--- script.js
+++ script.js
@@ -1,10 +1,47 @@
 $(window).on('resize', function() {
     var win = $(this);
-    if (win.width() >= 375) {
+    if (win.width() >= 554) {
+
+      $('#Nright').removeClass('justify-content-end');
+      $('#Nright').addClass('justify-content-center');
   
-      $('#123').addClass('btn-lg');
-  
-    } else {
-      $('#123').removeClass('btn-lg');
+    } 
+    else {
+      $('#Nright').addClass('justify-content-end');
     }
-  });
+});
+// Navbar
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
+// portfolio
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" w3-opacity-off", "");
+  }
+  x[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " w3-opacity-off";
+}
+
+// slides
+$('.carousel').carousel()
+
